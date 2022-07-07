0adf534f3a24c9df29d640d23d8ea0383a465b51
Uxa9 <kirill.yu99@gmail.com>
2022-07-08 02:16:32 +1000

    init

---
+++ terminal-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './styles/style.scss';
+
+import Button from './components/button';
+
+const App = () => {
+    return (
+        <Button
+            text="aboba"
+            type="decline"
+        />
+    );
+}
+
+export default App;
