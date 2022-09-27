dc44ad4c524e15004911174be2f300bb8168f0f7
Uxa9 <kirill.yu99@gmail.com>
2022-09-28 02:01:45 +1000

    wip

--- bnetex-server/src/users/users.controller.ts
+++ bnetex-server/src/users/users.controller.ts
@@ -34,7 +34,7 @@ export class UsersController {
     })
     @Roles('admin')
     @UseGuards(RolesGuard)
-    @Post()
+    @Get()
     getAll() {
         return this.userService.getAllUsers();
     }
