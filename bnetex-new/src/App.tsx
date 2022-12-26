38bbc9e7b09e86eca084e7d1572d5365d7c1bd30
Dmitry Duda <dmitrij.snake2000@yandex.ru>
2022-12-26 21:54:53 +1000

    add: dynamicImage avif & webp support

--- bnetex-new/src/App.tsx
+++ bnetex-new/src/App.tsx
@@ -3,11 +3,22 @@ import { CurrentPlatformProvider } from 'lib/hooks/usePlatform';
 import { PromiseWithLoadingProvider } from 'lib/hooks/usePromiseWithLoading';
 import { ThemeProvider } from 'lib/hooks/useTheme';
 import { ToastProvider } from 'lib/hooks/useToast';
+import { checkImageFormatSupport, ImageFormat } from 'lib/utils/checkImageFormatSupport';
 import ModalSpawner from 'modules/Global/components/ModalSpawn/ModalSpawner/modalSpawner';
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from 'routes/appRoutes';
 
 function App() {
+
+    useEffect(() => {
+        // Проверка наилучшего формата изображений, поддерживаемого браузером пользователя (avif, webp, png)
+        checkImageFormatSupport()
+            .then((format: ImageFormat) => localStorage.setItem('availableImageFormat', format));
+
+        return () => localStorage.removeItem('pendingFreeTimeRequest');
+    }, []);
+
     return (
         <BrowserRouter>
             <ThemeProvider>
