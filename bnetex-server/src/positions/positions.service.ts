459704ddfa659eefdd053f8b9cc4e2e9d6869ae8
Natural Code <evnikolaevv@gmail.com>
2022-12-29 15:13:06 +0300

    fix: pnl

--- bnetex-server/src/positions/positions.service.ts
+++ bnetex-server/src/positions/positions.service.ts
@@ -11,159 +11,157 @@ import { PositionEnters } from './positionEnters.model';
 
 @Injectable()
 export class PositionsService {
+  constructor(
+    // @InjectModel(Position.name) private PositionModel: Model<PositionDocument>
+    @InjectModel(Position) private positionRepository: typeof Position,
+    @InjectModel(PositionEnters)
+    private positionEntersRepository: typeof PositionEnters,
+    private readonly httpService: HttpService,
+  ) {}
 
-    constructor(
-        // @InjectModel(Position.name) private PositionModel: Model<PositionDocument>
-        @InjectModel(Position) private positionRepository: typeof Position,
-        @InjectModel(PositionEnters) private positionEntersRepository: typeof PositionEnters,
-        private readonly httpService: HttpService
-    ) { }
+  async getPnlAndRoe(dto: GetDataDto) {
+    const date = new Date().setMonth(new Date().getMonth() - dto.period);
 
-    async getPnlAndRoe(dto: GetDataDto) {
-        const date = new Date().setMonth(new Date().getMonth() - dto.period);
+    // const positions = await this.positionRepository.findAll({
+    //     where: {
+    //         // enterTime: { $gte: date },
+    //         closeTime: { [Op.ne]: null }
+    //     },
+    //     order: [['enterTime', "ASC"]]
+    // });
 
-        // const positions = await this.positionRepository.findAll({
-        //     where: {
-        //         // enterTime: { $gte: date },
-        //         closeTime: { [Op.ne]: null }
-        //     },
-        //     order: [['enterTime', "ASC"]]
-        // });
+    // const positions = await Promise.resolve(() => {
+    //     this.httpService.post('http://localhost:3009/front/history', {
+    //         periodMonth: 12
+    //     }).subscribe((res) => {
+    //         console.log(res);
+    //         return res;
+    //     });
+    // })
 
-        // const positions = await Promise.resolve(() => {
-        //     this.httpService.post('http://localhost:3009/front/history', {
-        //         periodMonth: 12
-        //     }).subscribe((res) => {
-        //         console.log(res);
-        //         return res;
-        //     });
-        // }) 
-        const positions = await new Promise((resolve, rej) => this.httpService.post('http://localhost:3009/front/history', {
-                periodMonth: 12
-            }).subscribe((res) => {
-                resolve(res.data.response);
-            }))
+    const positions = await new Promise((resolve, rej) =>
+      this.httpService
+        .post('http://localhost:3009/front/history', {
+          periodMonth: 12,
+        })
+        .subscribe((res) => {
+          resolve(res.data.response);
+        }),
+    );
 
-        // console.log(positions[0]);
+    const result = await this.getHistoricalData(positions, dto.amount);
+
+    return result;
+  }
+
+  async getHistoricalData(data: any, amount: number) {
+    let dates = [];
+    let pnlValues = [];
+    let roeValues = [];
+    let acc = amount;
+
+    data.map((position) => {
+
         
+      let customAmount = amount / position.deposit;
 
+      const lever = 10;
 
-        const result = await this.getHistoricalData(positions, dto.amount);
+      const percent = position.percentProfit;
 
-        return result;
-    }
+      const pnl = position.sumProfit * customAmount * lever;
 
-    async getHistoricalData(data: any, amount: number) {
+      if (
+        new Date(position.closeTime).getDate() !==
+          new Date(dates[dates.length - 1]).getDate() &&
+        new Date(position.closeTime).getMonth() !==
+          new Date(dates[dates.length - 1]).getMonth()
+      ) {
+        const date = new Date(position.closeTime);
+        dates.push(
+          `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`,
+        );
+        roeValues.push((roeValues[roeValues.length - 1] || 0) + percent);
+        pnlValues.push(pnl);
+      } else {
+        roeValues[roeValues.length - 1] += percent;
+        pnlValues[pnlValues.length - 1] += pnl;
+      }
 
-        let dates = [];
-        let pnlValues = [];
-        let roeValues = [];
-        let acc = amount;
+      acc += pnl;
+    });
 
-        data.map((position) => {
+    return {
+      dates,
+      pnlValues,
+      roeValues,
+    };
+  }
 
-            const lever = 10;
-            const percent = position.sumProfit / position.deposit * 100;
-            const x = position.volumeUSDT / position.deposit * acc;
-            const pnl = x * percent / 100 * lever;
-            console.log(new Date(position.closeTime).getDate());
-            
-            
+  async getHistoricalDataOrders(period: number) {
+    const date = new Date().setMonth(new Date().getMonth() - period);
 
-            if ( new Date(position.closeTime).getDate() !== 
-                 new Date(dates[dates.length - 1]).getDate() &&
-                 new Date(position.closeTime).getMonth() !== 
-                 new Date(dates[dates.length - 1]).getMonth()
-                ) {
+    const positions = await this.positionRepository.findAll({
+      where: {
+        enterTime: { $gte: date },
+        closeTime: { [Op.ne]: null },
+      },
+      // include posEnters
+    });
 
-                const date = new Date(position.closeTime);
-                dates.push(`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`);
-                roeValues.push((roeValues[roeValues.length - 1] || 0) + percent);
-                pnlValues.push(pnl);
-            } else {               
-                roeValues[roeValues.length - 1]+= percent
-                pnlValues[pnlValues.length - 1]+= pnl;
-            }               
+    let res = [];
 
-            // roeValues.push(roeValues[index - 1] + percent)
-            //     pnlValues.push(pnlValues[index - 1] + percent);
+    positions.map((position) => {
+      const { positionEnters } = position;
 
-            acc += pnl;
-        });
+      const lever = 10;
 
+      const enters = positionEnters.map((enter) => {
         return {
-            dates,
-            pnlValues,
-            roeValues
-        }
+          date: enter.createdAt,
+          action: 'purchase',
+          amount: enter.volumeUSDT * lever,
+          price: enter.close,
+          PNL: 0,
+        };
+      });
+
+      res = [...res, ...enters];
+
+      res.push({
+        date: position.closeTime,
+        action: position.positionType === 'LONG' ? 'sale' : 'purchase',
+        amount: position.volumeUSDT * lever,
+        price: position.closePrice,
+        PNL: position.sumProfit * lever,
+      });
+    });
+
+    return res;
+    return {};
+  }
+
+  async getCurrentOpenPosition() {
+    const position = await this.positionRepository.findOne({
+      where: {
+        closeTime: null,
+      },
+      order: [['enterTime', 'DESC']],
+    });
+
+    if (position) {
+      return position;
     }
 
-    async getHistoricalDataOrders(period: number) {
-        const date = new Date().setMonth(new Date().getMonth() - period);
+    return null;
+  }
 
-        const positions = await this.positionRepository.findAll({
-            where: {
-                enterTime: { $gte: date },
-                closeTime: { [Op.ne]: null }
-            }
-            // include posEnters
-        });
-
-        let res = [];
-
-        positions.map(position => {
-
-            const { positionEnters } = position;
-
-            const lever = 10;
-
-            const enters = positionEnters.map(enter => {
-                return {
-                    date: enter.createdAt,
-                    action: "purchase",
-                    amount: enter.volumeUSDT * lever,
-                    price: enter.close,
-                    PNL: 0
-                }
-            });
-
-            res = [...res, ...enters];
-
-            res.push({
-                date: position.closeTime,
-                action: position.positionType === "LONG" ? "sale" : "purchase",
-                amount: position.volumeUSDT * lever,
-                price: position.closePrice,
-                PNL: position.sumProfit * lever
-            });
-        });
-
-        return res;
-        return {};
-    }
-
-    async getCurrentOpenPosition() {
-
-        const position = await this.positionRepository.findOne({
-            where: {
-                closeTime: null
-            },
-            order: [['enterTime', "DESC"]]
-        });
-
-        if (position) {
-            return position;
-        }
-
-        return null;
-    }
-
-    async getLastClosedPosition() {
-        return await this.positionRepository.findOne({
-            where: {
-                closeTime: { [Op.ne]: null }
-            },
-            order: [['closeTime', "DESC"]]
-        });
-    }
+  async getLastClosedPosition() {
+    return await this.positionRepository.findOne({
+      where: {
+        closeTime: { [Op.ne]: null },
+      },
+      order: [['closeTime', 'DESC']],
+    });
+  }
 }
