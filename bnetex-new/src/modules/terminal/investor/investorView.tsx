dd354a7f6d63ced0bc20515681836eea66ccbb91
Uxa9 <kirill.yu99@gmail.com>
2023-01-12 01:17:51 +1000

    websocket for trading session

--- bnetex-new/src/modules/terminal/investor/investorView.tsx
+++ bnetex-new/src/modules/terminal/investor/investorView.tsx
@@ -8,13 +8,28 @@ import clsx from 'clsx';
 import SignedNumber from 'modules/Global/components/signedNumber/signedNumber';
 import Chart from 'modules/Global/components/lightChart/chart';
 import { useTypedSelector } from 'lib/hooks/useTypedSelector';
+import { getUserInfo } from 'lib/utils/getUserInfo';
+// import { io, Socket } from 'socket.io-client';
 
 type InvestorViewType = 'trade' | 'history';
 
 const InvestorView = () => {
 
     const [viewType, setViewType] = useState<InvestorViewType>('trade');
+    // const [investorPnl, setInvestorPnl] = useState<Number>(0);
+    // const [invesotrRoe, setInvestorRoe] = useState<Number>(0);
+
     const { dates, roe, pnl, loading } = useTypedSelector(state => state.roePnl);
+
+    // const tradeSession = io(`http://localhost:5000?id=${getUserInfo().userId}`);
+
+    // tradeSession.on('connect', () => {
+    //     console.log('connected'); 
+    // });
+
+    // tradeSession.on('currentPosition', (newMessage: any) => {
+    //     console.log(newMessage);
+    // });       
 
     return (
         <>
--- bnetex-server/src/gateway/gateway.module.ts
+++ bnetex-server/src/gateway/gateway.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { UsersModule } from "../users/users.module";
 import { SocketModule } from "../socket/socket.module";
 import { MyGateway } from "./gateway";
+import { InvestSessionsModule } from "../invest-sessions/invest-sessions.module";
 
 @Module({
     providers: [MyGateway],
-    imports: [SocketModule, UsersModule]
+    imports: [SocketModule, UsersModule, InvestSessionsModule]
 })
 export class GatewayModule {}
