35490cf229410c93e0830b4c4c2f5f53f7cae1fe
Strashko Maksim <sm@outsi.de>
2023-05-03 12:38:34 +1000

    chore: positions swagger

--- bnetex-server/src/positions/positions.controller.ts
+++ bnetex-server/src/positions/positions.controller.ts
@@ -1,23 +1,28 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { GetDataDto } from './dto/get-data.dto';
 import { PositionsService } from './positions.service';
 import { GetPositionsDto } from './dto/get-positions.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('positions')
+@ApiTags('Positions')
 export class PositionsController {
     constructor(private PositionService: PositionsService) {}
 
     @Get()
+    @ApiOperation({ summary: 'Get historical positions data' })
     getHistoricalDataOrders(@Query() dto: GetPositionsDto) {
         return this.PositionService.getHistoricalDataOrders(dto);
     }
 
-    @Post('/getHistData')
+    @Post('getHistData')
+    @ApiOperation({ summary: 'Get histograms data' })
     getData(@Body() dto: GetDataDto) {
         return this.PositionService.getPnlAndRoe(dto);
     }
 
-    @Post('/getTVDAta')
+    @Post('getTVDAta')
+    @ApiOperation({ summary: 'Get TV data' })
     getTVDAta(@Body() dto: any) {
         return this.PositionService.getTVdata(dto);
     }
